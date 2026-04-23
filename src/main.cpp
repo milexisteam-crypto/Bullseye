941368424cd3949d5886afd3e736b32b5e4992ae
milexisteam-crypto <milexisteam@gmail.com>
2026-04-23 17:05:52 +0200

    dfg

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,8 @@ extern "C" void kmain(void) {
     struct limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
 
     for (size_t i = 0; i < 100; i++) {
-        // z jakiegokolwiek powodu to nie dzialaaa
         volatile uint32_t *fb_ptr = (uint32_t*)framebuffer->address;
-        fb_ptr[i * (framebuffer->pitch / 4) + i] = 0x2233;
+        fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xfffff;
     }
 
     __asm__("hlt");
