c2e3038d8e3dc13c15f90b11cc5896c0268eb2f0
milexisteam-crypto <milexisteam@gmail.com>
2026-04-24 16:04:30 +0200

    text

--- obj/ps2/ps2keyboard.cpp.d
+++ obj/ps2/ps2keyboard.cpp.d
@@ -1,3 +1,4 @@
 obj/ps2/ps2keyboard.cpp.o: src/ps2/ps2keyboard.cpp \
- src/include/ps2keyboard.hpp
+ src/include/ps2keyboard.hpp src/include/utils.hpp
 src/include/ps2keyboard.hpp:
+src/include/utils.hpp:
